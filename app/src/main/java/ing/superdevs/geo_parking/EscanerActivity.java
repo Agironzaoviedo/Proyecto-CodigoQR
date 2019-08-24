4e94f942de78e49f90ab83c65c6b2156f3ac9919
Anderson <anderson111cristo@gmail.com>
2019-08-24 15:53:02 -0500

    agregue el lector

---
+++ app/src/main/java/ing/superdevs/geo_parking/EscanerActivity.java
@@ -0,0 +1,14 @@
+package ing.superdevs.geo_parking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EscanerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escaner);
+    }
+}
